3d296ff4e2fde11baff00567ef1b84f088c33f09
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-10 15:43:58 +0530

    commit

--- +_-_0_in_array.c
+++ +_-_0_in_array.c
@@ -1,2 +1,29 @@
 // Count positive, negative, and zero elements in an array.
+#include <stdio.h>
+int main() {
+    int n, i;
+    int positive_count = 0, negative_count = 0, zero_count = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] > 0) {
+            positive_count++;
+        } else if (arr[i] < 0) {
+            negative_count++;
+        } else {
+            zero_count++;
+        }
+    }
+
+    printf("Positive elements: %d\n", positive_count);
+    printf("Negative elements: %d\n", negative_count);
+    printf("Zero elements: %d\n", zero_count);
+
+    return 0;
+}
 
