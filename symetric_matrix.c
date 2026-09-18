76dec64e6586b3b6ec83219d24df4656f860fcd8
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-18 22:32:53 +0530

    commit

--- symetric_matrix.c
+++ symetric_matrix.c
@@ -1,3 +1,39 @@
 //Check if a matrix is symmetric.
+#include <stdio.h>
 
- 
+int main() {
+    int matrix[10][10];
+    int n, i, j;
+    int isSymmetric = 1;
+
+    printf("Enter the size of the square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check if the matrix is symmetric
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+        if (!isSymmetric) {
+            break;
+        }
+    }
+
+    if (isSymmetric) {
+        printf("The matrix is symmetric.\n");
+    } else {
+        printf("The matrix is not symmetric.\n");
+    }
+
+    return 0;
+}
