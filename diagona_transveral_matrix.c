a5b8999e77a32a9ef13a4977a77eefcaf8df2abd
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-18 22:29:38 +0530

    commit

--- diagona_transveral_matrix.c
+++ diagona_transveral_matrix.c
@@ -1,1 +1,27 @@
 //Perform diagonal traversal of a matrix.
+#include <stdio.h>
+int main() {
+    int a[10][10], m, n, i, j;
+
+    printf("Enter dimensions of matrix: ");
+    scanf("%d %d", &m, &n);
+
+    printf("Enter elements of matrix:\n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Diagonal traversal of the matrix:\n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            if (i == j) {
+                printf("%d ", a[i][j]);
+            }
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
