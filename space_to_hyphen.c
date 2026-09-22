22631553a424b5038bed475c8218341f7094e5d8
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-22 11:27:57 +0530

    commit

--- space_to_hyphen.c
+++ space_to_hyphen.c
@@ -1,1 +1,18 @@
 // Replace spaces with hyphens in a string.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    printf("String after replacing spaces with hyphens: %s", str);
+    return 0;
+}
