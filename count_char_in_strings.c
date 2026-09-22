30e150fd769f9ca90be25a1996a42d3406de8a65
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-22 11:28:15 +0530

    commit

--- count_char_in_strings.c
+++ count_char_in_strings.c
@@ -1,1 +1,28 @@
 // Count spaces, digits, and special characters in a string.
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == ' ') {
+            spaces++;
+        } else if (isdigit(str[i])) {
+            digits++;
+        } else {
+            special++;
+        }
+    }
+
+    printf("Spaces: %d\n", spaces);
+    printf("Digits: %d\n", digits);
+    printf("Special characters: %d\n", special);
+
+    return 0;
+}
