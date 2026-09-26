fe325aabc8b198a543164904afd4c51255c801ff
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 14:02:58 +0530

    commit

--- toggle_case_of_char.c
+++ toggle_case_of_char.c
@@ -1,1 +1,11 @@
 // Toggle case of each character in a string
+#include <stdio.h>
+void toggleCase(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A'; // Convert to uppercase
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] - 'A' + 'a'; // Convert to lowercase
+        }
+    }
+}
