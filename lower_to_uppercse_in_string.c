addcb1ff1dfecef2c91a4b319f25d3bf2e951978
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 13:56:47 +0530

    commit

--- lower_to_uppercse_in_string.c
+++ lower_to_uppercse_in_string.c
@@ -1,1 +1,18 @@
 //Convert a lowercase string to uppercase without using built-in functions.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a lowercase string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        }
+    }
+
+    printf("Uppercase string: %s", str);
+    return 0;
+}
