7670c29605f40a315bd6e9e602ceeec061599e9c
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 14:03:18 +0530

    commit

--- reverse_a_string.c
+++ reverse_a_string.c
@@ -1,1 +1,16 @@
 // Reverse a string
+#include <stdio.h>
+void reverseString(char *str) {
+    char *start = str;
+    char *end = str;
+    while (*end != '\0') {
+        end++;
+    }
+    end--; // Move back to the last character
+
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
