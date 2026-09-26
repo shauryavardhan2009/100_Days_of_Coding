fa80dd98dd62731b9645a6e0d4e8e79c2e1c2805
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 14:01:15 +0530

    commit

--- longest_word.c
+++ longest_word.c
@@ -1,1 +1,24 @@
 // Find the longest word in a sentence.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char sentence[1000];
+    char longestWord[100] = "";
+    char *word;
+
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    word = strtok(sentence, " \n");
+    while (word != NULL) {
+        if (strlen(word) > strlen(longestWord)) {
+            strcpy(longestWord, word);
+        }
+        word = strtok(NULL, " \n");
+    }
+
+    printf("Longest word: %s\n", longestWord);
+
+    return 0;
+}
