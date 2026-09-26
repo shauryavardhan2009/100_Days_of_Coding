844f3a9b8eb465935a15b12c2c85376579acb759
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 13:57:46 +0530

    commit

--- pallindrome_string.c
+++ pallindrome_string.c
@@ -1,1 +1,27 @@
 // Check if a string is a palindrome.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = strlen(str);
+    int is_palindrome = 1;
+
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            is_palindrome = 0;
+            break;
+        }
+    }
+
+    if (is_palindrome) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
