f51395f9199f2208b49c631d96323d325955742e
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 13:58:13 +0530

    commit

--- frequency_of_number_in_string.c
+++ frequency_of_number_in_string.c
@@ -1,1 +1,25 @@
 // Count frequency of a given character in a string
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    char target;
+    int frequency = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter a character to count: ");
+    scanf("%c", &target);
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == target) {
+            frequency++;
+        }
+    }
+
+    printf("Frequency of '%c': %d\n", target, frequency);
+
+    return 0;
+}
