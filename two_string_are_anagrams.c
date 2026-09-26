9f343c3b21c28fadb1c88ef1cd52f38c4b4d43ab
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 14:01:35 +0530

    commit

--- two_string_are_anagrams.c
+++ two_string_are_anagrams.c
@@ -1,1 +1,23 @@
 // Check if two strings are anagrams of each other.
+#include <stdio.h>
+int areAnagrams(char *str1, char *str2) {
+    int count[256] = {0};
+    int i;
+
+    for (i = 0; str1[i] && str2[i]; i++) {
+        count[(unsigned char)str1[i]]++;
+        count[(unsigned char)str2[i]]--;
+    }
+
+    if (str1[i] || str2[i]) {
+        return 0; // Strings are of different lengths
+    }
+
+    for (i = 0; i < 256; i++) {
+        if (count[i] != 0) {
+            return 0; // Not anagrams
+        }
+    }
+
+    return 1; // Anagrams
+}
