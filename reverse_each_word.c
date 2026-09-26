bb8cda8a3c7068d0c88fa16a0790c441ea63f50a
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 13:58:39 +0530

    commit

--- reverse_each_word.c
+++ reverse_each_word.c
@@ -1,1 +1,9 @@
 // Reverse each word in a sentence without changing the word order.
+#include <stdio.h>
+void reverseWord(char *start, char *end) {
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
