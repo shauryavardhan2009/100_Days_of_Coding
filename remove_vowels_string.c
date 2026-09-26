1b17ff41c0bd09e0bd7451fc306555648fd509f7
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-26 14:02:21 +0530

    commit

--- remove_vowels_string.c
+++ remove_vowels_string.c
@@ -1,1 +1,13 @@
 // Remove all vowels from a string
+#include <stdio.h>
+void removeVowels(char *str) {
+    char *src = str, *dst = str;
+    while (*src) {
+        if (*src != 'a' && *src != 'e' && *src != 'i' && *src != 'o' && *src != 'u' &&
+            *src != 'A' && *src != 'E' && *src != 'I' && *src != 'O' && *src != 'U') {
+            *dst++ = *src;
+        }
+        src++;
+    }
+    *dst = '\0';
+}
